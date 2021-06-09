1e4b8176f133e41eda538270440140e9dd37d16b
dan-goo <80808228+dan-goo@users.noreply.github.com>
2021-06-09 14:58:25 +0900

    범브 게임 소리랑 에너미 터지기

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,15 @@ public class PlayerController : MonoBehaviour
     private void Start() { ani = GetComponent<Animator>(); }
     void Update()
     {
+        if (GameManager.instance.isGameover) { return; }
         translation = Input.GetAxis("Vertical") * 10f; rotation = Input.GetAxis("Horizontal") * 100f;
         transform.Translate(0, 0, translation * Time.deltaTime); transform.Rotate(0, rotation * Time.deltaTime, 0);
         if (translation != 0 || rotation != 0) { ani.SetBool("walk", true); } else { ani.SetBool("walk", false); }
     }
-    private void OnCollisionEnter(Collision collision) { if (collision.collider.tag == "Obstacle") { ani.SetTrigger("damage");
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if (GameManager.instance.isGameover) { return; }
+        if (collision.collider.tag == "Obstacle") { ani.SetTrigger("damage");
             GameManager.instance.AddScore(-1);
         }
         if (collision.collider.tag == "Enermy")
